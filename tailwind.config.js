2afadf36a01e86de02edbfcf393e867de14becf2
antondomratchev <anton@domratchev.dev>
2023-09-19 00:19:13 -0500

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FD4F00",
+      }
+    },
+  },
+  plugins: [],
+}
+
