f79deca6caffaa3d71e864274ff82f30f556298b
antondomratchev <anton@domratchev.dev>
2023-09-19 21:31:38 -0500

    final

--- src/components/Light.jsx
+++ src/components/Light.jsx
@@ -6,29 +6,46 @@ const up = new URL('../images/up.svg', import.meta.url);
 const lightOn = new URL('../images/light-on.svg', import.meta.url);
 
 export const Light = () => {
+    const [brightness, setBrightness] = React.useState(10)
+    const decreaseBrightness = () => { 
+        setBrightness((currentBrightness) => {
+            if (currentBrightness == 0) {
+                return currentBrightness
+            }
+            return currentBrightness - 10
+        }) 
+    }
+    const increaseBrightness = () => { 
+        setBrightness((currentBrightness) => {
+            if (currentBrightness == 100) {
+                return currentBrightness
+            }
+            return currentBrightness + 10
+        }) 
+    }
     return (
         <div>
             <h1>Front Porch Light</h1>
             <div id="light">
                 <div class="meter">
-                    <span>
-                    0%
+                    <span style={{ width: `${brightness}%`}}>
+                    {brightness} %
                     </span>
                 </div>
 
-                <button>
+                <button onClick={() => { setBrightness(0)}}>
                     <img src={lightOff} />
                 </button>
 
-                <button>
+                <button onClick={decreaseBrightness}>
                     <img src={down} />
                 </button>
 
-                <button>
+                <button onClick={increaseBrightness}>
                     <img src={up} />
                 </button>
 
-                <button>
+                <button onClick={() => { setBrightness(100)}}>
                     <img src={lightOn} />
                 </button>
             </div>
